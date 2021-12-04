51f4151ca049637efa073ab00a54195bb4b16b52
iryzhank <i.d.ryzhenkov@gmail.com>
2021-12-04 15:44:50 +0100

    Adding Consts

---
+++ src/database/Const.java
@@ -0,0 +1,14 @@
+package database;
+
+/**
+ *
+ * @author Ivan Ryzhankow, Alina Yermakova
+ */
+public class Const 
+{
+    private Const(){}
+    
+    public static final int NA_REPLACEMENT_INT = 2000;
+    public static final double NA_REPLACEMENT_DOUBLE = 2000.0;
+    public static final double LB_TO_KG_RATIO = 0.45359237; 
+}
